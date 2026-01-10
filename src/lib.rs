85f5c39c60aa8ce38c27390c481a8dc213508061
Essentia Development <jshiers@appabacus.com>
2026-01-10 20:39:06 +0000

    refactor(lib): Remove unnecessary clippy warnings from the main module

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,11 @@
 //! Implements the decentralized e-commerce and affiliate platform logic,
 //! including the Genesis Business Directory Node.
 
+#![allow(clippy::unnecessary_literal_bound)]
+#![allow(clippy::uninlined_format_args)]
+#![allow(clippy::cast_lossless)]
+#![allow(clippy::struct_excessive_bools)]
+
 pub mod errors;
 pub mod r#impl;
 pub mod traits;
